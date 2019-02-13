dc236dda7469f99b026d29a417825dec1edd00c6
Pavel Zanchuk <buzzik@yahoo.com>
2019-02-12 21:31:35 -0800

    inital setup

---
+++ QueenAttack.Tests/ModelTests/QueenTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace QueenAttack.Tests
+{
+  [TestClass]
+  public class QueenTest
+  {
+    // Test methods go here
+  }
+}
---
+++ QueenAttack/Models/Queen.cs
@@ -0,0 +1,8 @@
+namespace QueenAttack
+{
+  public class Queen
+  {
+    // properties, constructors, methods, etc. go here
+    }
+  }
+}
